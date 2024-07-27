b3a75f47e44b37e0cbbc79a0e60f0627e2f018c5
MeetWithRajat <meetwithrajat@gmail.com>
2024-07-28 00:09:47 +0530

    book list

--- books.py
+++ books.py
@@ -4,6 +4,21 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/api-endpoint")
-async def first_api():
-    return {"message": "Hello Rajat"}
+books = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'Science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'Science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'History'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'Mathematics'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'Mathematics'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'Mathematics'},
+]
+
+
+@app.get("/hello")
+async def welcome():
+    return "Hello Rajat"
+
+
+@app.get("/books")
+async def read_all_books():
+    return {"book list": books}
